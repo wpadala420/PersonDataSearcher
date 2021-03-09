3dca6d8b6226a222c6dec4342abea76029acff0a
Wojciech Padała <wojciech.padala@skanuj.to>
2021-03-10 00:57:19 +0100

    dump targets pages for tests and change profiles gathering

--- modules/FacebookPublicAccountParser.py
+++ modules/FacebookPublicAccountParser.py
@@ -16,6 +16,24 @@ class FacebookPublicAccountParser:
         self.email = email
         self.password = password
 
+    def getUrlFromHref(self, href):
+        result = ''
+        found_first_equal_mark = False
+        first_equal_index = -1
+        for c in range(len(href)):
+            if href[c] == '=' and found_first_equal_mark is False:
+                found_first_equal_mark = True
+                first_equal_index = c
+        if first_equal_index != -1:
+            for it in range(first_equal_index + 2, len(href)):
+                if href[it] != '"':
+                    result += href[it]
+                else:
+                    return result
+
+
+
+
     def login(self, session, email, password):
         response = session.get('https://m.facebook.com')
         response = session.post('https://m.facebook.com/login.php', data={
@@ -32,11 +50,11 @@ class FacebookPublicAccountParser:
             return False
 
     def nonLoggingSearch(self, name):
-        url = 'https://facebook.com/public/'
+        base_url = 'https://m.facebook.com'
         nameAndSurname = name.split(' ')
         firstName = nameAndSurname[0]
         lastName = nameAndSurname[1]
-        url = url + firstName + '+' + lastName
+        url = base_url + '/public/' + firstName + '+' + lastName
         cont = get(url, headers={':authority:': 'www.facebook.com', ':method:': 'GET', ':path:': '/public/emil+wróbel',
                                  ':scheme:': 'https',
                                  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
@@ -58,6 +76,9 @@ class FacebookPublicAccountParser:
                 line = str(line, encoding='utf-8')
                 if line.find('hidden_elem') != -1 and line.startswith('<div class="hidden_elem">'):
                     linesWithData.append(line)
+                elif line.find('href="/') != -1:
+                    linesWithData.append(line)
+
         tags = []
         for elem in linesWithData:
             for i in range(len(elem)):
@@ -77,13 +98,10 @@ class FacebookPublicAccountParser:
         for href in hrefs:
             split = href.split(' ')
             for part in split:
-                if part.find('href') != -1:
-                    href_split = part.split('=')
-                    for hs in href_split:
-                        if hs.find('http') != -1:
-                            corrected_link = hs.replace('>', '').replace('"', '')
-                            if corrected_link.endswith('/photos') is False:
-                                links.append(corrected_link)
+                if part.find('href') != -1 and part.find('https://') == -1 and part.find('=https') == -1 and (part.lower().find(nameAndSurname[0].lower()) != -1 or part.lower().find(nameAndSurname[1].lower()) != -1):
+                    corrected_link = self.getUrlFromHref(part)
+                    if corrected_link is not None and corrected_link.endswith('/photos') is False:
+                        links.append(base_url + corrected_link)
         links = set(links)
         return links
 
@@ -96,6 +114,7 @@ class FacebookPublicAccountParser:
         if b:
             print(a + ':' + b + ':' + c)
             links = self.nonLoggingSearch(name)
+            count = 0
             for link in links:
                 if link.find('people') == -1:
                     print(link.replace('www.', 'm.'))
@@ -104,6 +123,9 @@ class FacebookPublicAccountParser:
                     var = new_sess.content
                     var_t = new_sess.text
                     str_var = str(var, encoding='utf-8')
+                    # with open( str(count) + '.txt', 'wb') as tmp_file:
+                    #     tmp_file.write(bytes(str_var, encoding='utf-8'))
+                    #     count += 1
                     # print(str_var)
                     soup = bs4.BeautifulSoup(str_var, 'html.parser')
                     about_tags = soup.find_all()
@@ -121,5 +143,5 @@ class FacebookPublicAccountParser:
 
 
 if __name__ == '__main__':
-    fb = FacebookPublicAccountParser('vojtekk94@o2.pl', 'kochampalictrawke')
-    fb.loggingSearch('Damian Rusinek')
+    fb = FacebookPublicAccountParser()
+    fb.loggingSearch('Emil Wróbel')
