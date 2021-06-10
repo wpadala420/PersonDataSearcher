c53e925f4e3234f1ea26c5ce306d9fda4a76e1ee
Wojciech Padała <wojciech.padala@skanuj.to>
2021-06-10 12:48:33 +0200

    add getting data prof articles json

--- modules/articles.py
+++ modules/articles.py
@@ -7,5 +7,13 @@ def search_articles(name):
     })
     url = 'https://www.etools.ch/partnerSearch.do?partner=Carrot2Json&query={}&dataSourceResults=400&maxRecords=200&safeSearch=true&dataSources=all&language=all&country=web'.format(name)
     data = session.get(url).json()
+    data_list = []
+    if 'response' in data:
+        if 'mergedRecords' in data['response']:
+            for record in data['response']['mergedRecords']:
+                elem = {}
+                elem['title'] = record['title']
+                elem['url'] = record['url']
+                elem['text'] = record['text']
+                data_list.append(elem)
     return data
-
